113676467835168d084f19286ac5e7e125cf2acc
Lonka-Pardhu <lonkapardhu99@gmail.com>
2024-01-17 18:19:55 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SideNavbar from './components/SideNav'
+function App() {
+
+  return (
+    <>
+      <SideNavbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,7 @@
+export default function SideNav() {
+    return (
+        <>
+            <h1>Rapid Quest</h1>
+        </>
+    )
+}
